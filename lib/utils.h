c40fb84b3fa6dc169cae70e9b48e7d868ba6cb2d
Tomas Winkler <tomas.winkler@intel.com>
2020-07-02 20:38:50 +0300

    igsc: lib: memcpy_s more complete implemenation
    Add more complete implemenation for igsc_memcpy_s()
    wrapper.
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- lib/utils.h
+++ lib/utils.h
@@ -15,11 +15,27 @@
 #endif /* __linux__ */
 
 #ifdef __linux__
+#include <limits.h>
+#define RSIZE_MAX LONG_MAX
 static inline int gsc_memcpy_s(void *dest, size_t dest_size,
                                const void *src, size_t count)
 {
-    if (count > dest_size)
+    if (!dest || dest_size > RSIZE_MAX)
+    {
         return -1;
+    }
+
+    if (!src || dest_size < count)
+    {
+        memset(dest, 0, dest_size);
+        return -1;
+    }
+
+    if (((src >= dest) && ((char *)src < ((char *)dest + dest_size))) ||
+        ((src < dest) && ((char *)src + count - 1 >= (char *)dest)))
+    {
+        return -1;
+    }
 
     memcpy(dest, src, count);
     return 0;
