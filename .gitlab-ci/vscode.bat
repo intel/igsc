42379b265c2605477bb1c387a22b79b4065eb9e3
Tomas Winkler <tomas.winkler@intel.com>
2020-07-15 15:25:59 +0300

    igsc: ci: add visual studio build script for releases
    Add vscode build script for release.
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

---
+++ .gitlab-ci/vscode.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set path=%path%;"D:\Tools\ISDC_Tools\Compilers\cmake-3.16.4-win64-x64\bin"
+
+set VS_CODE="Visual Studio 16 2019"
+set CMAKE_ARGS="-DLIBMETEE_REPO=ssh://git@gitlab.devtools.intel.com:29418/mesw/metee.git"
+cmake %CMAKE_ARGS% -G %VS_CODE% -S . -B build
+cmake --build build --config Release -t package
+
