6ff792d96c99122b206909f3f175cab529ad266c
Alexander Usyskin <alexander.usyskin@intel.com>
2020-07-19 14:02:44 +0300

    igsc: lib: windows: retrieve bdf gfx device
    Retrieve bus device function (BDF) from the graphics device properties
    together with other device parameters.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- lib/enum/igsc_enum_windows.c
+++ lib/enum/igsc_enum_windows.c
@@ -104,9 +104,51 @@ void igsc_device_iterator_destroy(struct igsc_device_iterator *iter)
     free(iter);
 }
 
-static int gsc_get_property(const PWCHAR deviceInterfaceList,
-                            struct igsc_device_info *info)
+static int gsc_dev_get_property(DEVINST devInst, const DEVPROPKEY *PropertyKey,
+                                BYTE **Property)
 {
+    CONFIGRET cr;
+    ULONG PropertySize = 0;
+    DEVPROPTYPE PropType = 0;
+
+    cr = CM_Get_DevNode_PropertyW(devInst,
+                                  PropertyKey,
+                                  &PropType,
+                                  NULL,
+                                  &PropertySize,
+                                  0);
+    if (cr != CR_BUFFER_SMALL || (PropType != DEVPROP_TYPE_STRING))
+    {
+        gsc_error("Error 0x%x retrieving device property size.\n", cr);
+        return IGSC_ERROR_INTERNAL;
+    }
+
+    *Property = (BYTE *)calloc(PropertySize, sizeof(BYTE));
+    if (*Property == NULL)
+    {
+        gsc_error("Error allocating memory for device property.\n");
+        return IGSC_ERROR_NOMEM;
+    }
+
+    cr = CM_Get_DevNode_PropertyW(devInst,
+                                  PropertyKey,
+                                  &PropType,
+                                  *Property,
+                                  &PropertySize,
+                                  0);
+    if (cr != CR_SUCCESS || (PropType != DEVPROP_TYPE_STRING))
+    {
+        gsc_error("Error 0x%x retrieving device property.\n", cr);
+        free(*Property);
+        return IGSC_ERROR_INTERNAL;
+    }
+    return IGSC_SUCCESS;
+}
+
+static int gsc_get_properties(const PWCHAR deviceInterfaceList,
+                              struct igsc_device_info *info)
+{
+    int ret;
     CONFIGRET cr;
     BYTE DevID[MAX_DEVICE_ID_LEN];
     BYTE *Property;
@@ -114,6 +156,7 @@ static int gsc_get_property(const PWCHAR deviceInterfaceList,
     ULONG PropertySize = 0;
     DEVPROPTYPE PropType;
     DEVINST devInst;
+    DEVINST devParent;
 
     PropertySize = sizeof(DevID);
     cr = CM_Get_Device_Interface_PropertyW(deviceInterfaceList,
@@ -135,58 +178,55 @@ static int gsc_get_property(const PWCHAR deviceInterfaceList,
         return IGSC_ERROR_INTERNAL;
     }
 
-    PropertySize = 0;
-    cr = CM_Get_DevNode_PropertyW(devInst,
-                                  &DEVPKEY_Device_Parent,
-                                 &PropType,
-                                 NULL,
-                                 &PropertySize,
-                                 0);
-    if (cr != CR_BUFFER_SMALL || (PropType != DEVPROP_TYPE_STRING))
+    ret = gsc_dev_get_property(devInst, &DEVPKEY_Device_Parent, &Property);
+    if (ret == IGSC_SUCCESS)
     {
-        gsc_error("Error 0x%x retrieving device property size.\n", cr);
-        return IGSC_ERROR_INTERNAL;
+        gsc_debug("Parent Property %ws.\n", (WCHAR *)Property);
+        p = wcsstr((WCHAR *)Property, L"VEN_");
+        if (p)
+        {
+            info->vendor_id = (uint16_t)wcstol(p + 4, NULL, 16);
+        }
+        p = wcsstr((WCHAR *)Property, L"DEV_");
+        if (p)
+        {
+             info->device_id = (uint16_t)wcstol(p + 4, NULL, 16);
+        }
+        p = wcsstr((WCHAR *)Property, L"SUBSYS_");
+        if (p && wcslen(p) > 15) {
+            info->subsys_vendor_id = (uint16_t)wcstol(p + 11, NULL, 16);
+            p[11] = '\0';
+            info->subsys_device_id = (uint16_t)wcstol(p + 7, NULL, 16);
+        }
+        free(Property);
     }
 
-    Property = (BYTE *)malloc(PropertySize);
-    if (Property == NULL)
+    cr = CM_Get_Parent(&devParent, devInst, 0);
+    if (cr == CR_SUCCESS)
     {
-        gsc_error("Error allocating memory for device property.\n");
-        return IGSC_ERROR_NOMEM;
-    }
-    ZeroMemory(Property, PropertySize);
-
-    cr = CM_Get_DevNode_PropertyW(devInst,
-                                  &DEVPKEY_Device_Parent,
-                                  &PropType,
-                                  Property,
-                                  &PropertySize,
-                                  0);
-    if (cr != CR_SUCCESS || (PropType != DEVPROP_TYPE_STRING))
-    {
-        gsc_error("Error 0x%x retrieving device property.\n", cr);
-        return IGSC_ERROR_INTERNAL;
-    }
-
-    gsc_debug("Parent Property %ws.\n", (WCHAR *)Property);
-    p = wcsstr((WCHAR *)Property, L"VEN_");
-    if (p)
-    {
-        info->vendor_id = (uint16_t)wcstol(p + 4, NULL, 16);
-    }
-    p = wcsstr((WCHAR *)Property, L"DEV_");
-    if (p)
-    {
-        info->device_id = (uint16_t)wcstol(p + 4, NULL, 16);
-    }
-    p = wcsstr((WCHAR *)Property, L"SUBSYS_");
-    if (p && wcslen(p) > 15) {
-        info->subsys_vendor_id = (uint16_t)wcstol(p + 11, NULL, 16);
-        p[11] = '\0';
-        info->subsys_device_id = (uint16_t)wcstol(p + 7, NULL, 16);
+        ret = gsc_dev_get_property(devParent, &DEVPKEY_Device_LocationInfo, &Property);
+        if (ret == IGSC_SUCCESS)
+        {
+            gsc_debug("Location Property %ws.\n", (WCHAR *)Property);
+            p = wcsstr((WCHAR *)Property, L"PCI bus ");
+            if (p)
+            {
+                info->bus = (uint8_t)wcstol(p + 8, NULL, 10);
+            }
+            p = wcsstr((WCHAR *)Property, L"device ");
+            if (p)
+            {
+                info->dev = (uint8_t)wcstol(p + 7, NULL, 10);
+            }
+            p = wcsstr((WCHAR *)Property, L"function ");
+            if (p)
+            {
+                info->func = (uint8_t)wcstol(p + 9, NULL, 10);
+            }
+            free(Property);
+        }
     }
 
-    free(Property);
     return IGSC_SUCCESS;
 }
 
@@ -209,7 +249,7 @@ int igsc_device_iterator_next(struct igsc_device_iterator *iter,
     ZeroMemory(info, sizeof(*info));
     wcstombs_s(NULL, info->name, IGSC_INFO_NAME_SIZE - 1,
                iter->deviceInterface, IGSC_INFO_NAME_SIZE - 1);
-    ret = gsc_get_property(iter->deviceInterface, info);
+    ret = gsc_get_properties(iter->deviceInterface, info);
     iter->deviceInterface += wcslen(iter->deviceInterface) + 1;
 
     return ret;
@@ -227,5 +267,5 @@ int get_device_info_by_devpath(const char *devpath, struct igsc_device_info *inf
         return IGSC_ERROR_INTERNAL;
     }
 
-    return gsc_get_property(deviceInterface, info);
+    return gsc_get_properties(deviceInterface, info);
 }
