7834d64028fcdb0f40d10d04d3e83684400a269c
Vitaly Lubart <vitaly.lubart@intel.com>
2020-08-10 17:42:52 +0300

    igsc: tests: oprom version compare ut split tests
    Split tests to individual functions, remove duplicated one.
    
    Signed-off-by: Vitaly Lubart <vitaly.lubart@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- tests/oprom_tests.c
+++ tests/oprom_tests.c
@@ -167,8 +167,7 @@ static void test_params_version_non_compatible(void **state)
                      IGSC_VERSION_NOT_COMPATIBLE);
 }
 
-
-static void test_params_version_compare(void **state)
+static void test_params_version_older(void **state)
 {
     struct compare_version img_ver;
     struct compare_version dev_ver;
@@ -184,28 +183,76 @@ static void test_params_version_compare(void **state)
     assert_int_equal(igsc_oprom_version_compare((const struct igsc_oprom_version *)&img_ver,
                                                 (const struct igsc_oprom_version *)&dev_ver),
                      IGSC_VERSION_OLDER);
+}
+
+static void test_params_version_newer_minor(void **state)
+{
+    struct compare_version img_ver;
+    struct compare_version dev_ver;
+
+    memset(&img_ver, 0, sizeof(img_ver));
+    memset(&dev_ver, 0, sizeof(dev_ver));
+
+    img_ver.major = 19;
+    dev_ver.major = 19;
 
     img_ver.minor = 3;
     dev_ver.minor = 2;
     assert_int_equal(igsc_oprom_version_compare((const struct igsc_oprom_version *)&img_ver,
                                                 (const struct igsc_oprom_version *)&dev_ver),
                      IGSC_VERSION_NEWER);
+}
+
+static void test_params_version_newer_build1(void **state)
+{
+    struct compare_version img_ver;
+    struct compare_version dev_ver;
+
+    memset(&img_ver, 0, sizeof(img_ver));
+    memset(&dev_ver, 0, sizeof(dev_ver));
+
+    img_ver.major = 19;
+    dev_ver.major = 19;
+
+    img_ver.minor = 2;
+    dev_ver.minor = 2;
+    img_ver.build = 100;
+    dev_ver.build = 10;
+    assert_int_equal(igsc_oprom_version_compare((const struct igsc_oprom_version *)&img_ver,
+                                                (const struct igsc_oprom_version *)&dev_ver),
+                     IGSC_VERSION_NEWER);
+}
+
+static void test_params_version_newer_build2(void **state)
+{
+    struct compare_version img_ver;
+    struct compare_version dev_ver;
+
+    memset(&img_ver, 0, sizeof(img_ver));
+    memset(&dev_ver, 0, sizeof(dev_ver));
+
+    img_ver.major = 19;
+    dev_ver.major = 19;
 
     img_ver.minor = 2;
     dev_ver.minor = 2;
     img_ver.build = 10;
-    dev_ver.build = 12;
+    dev_ver.build = 100;
     assert_int_equal(igsc_oprom_version_compare((const struct igsc_oprom_version *)&img_ver,
                                                 (const struct igsc_oprom_version *)&dev_ver),
                      IGSC_VERSION_NEWER);
+}
 
-    img_ver.minor = 2;
-    dev_ver.minor = 2;
-    img_ver.build = 10;
-    dev_ver.build = 12;
-    assert_int_equal(igsc_oprom_version_compare((const struct igsc_oprom_version *)&img_ver,
-                                                (const struct igsc_oprom_version *)&dev_ver),
-                     IGSC_VERSION_NEWER);
+static void test_params_version_newer_equal(void **state)
+{
+    struct compare_version img_ver;
+    struct compare_version dev_ver;
+
+    memset(&img_ver, 0, sizeof(img_ver));
+    memset(&dev_ver, 0, sizeof(dev_ver));
+
+    img_ver.major = 19;
+    dev_ver.major = 19;
 
     img_ver.minor = 2;
     dev_ver.minor = 2;
@@ -215,6 +262,7 @@ static void test_params_version_compare(void **state)
                                                 (const struct igsc_oprom_version *)&dev_ver),
                      IGSC_VERSION_EQUAL);
 }
+
 int main(void)
 {
     const struct CMUnitTest tests[] = {
@@ -232,7 +280,11 @@ int main(void)
     const struct CMUnitTest version_cmp_tests[] = {
         cmocka_unit_test(test_params_version_null),
         cmocka_unit_test(test_params_version_non_compatible),
-        cmocka_unit_test(test_params_version_compare),
+        cmocka_unit_test(test_params_version_older),
+        cmocka_unit_test(test_params_version_newer_minor),
+        cmocka_unit_test(test_params_version_newer_build1),
+        cmocka_unit_test(test_params_version_newer_build2),
+        cmocka_unit_test(test_params_version_newer_equal),
     };
 
     int status = cmocka_run_group_tests(tests, group_setup, group_teardown);
