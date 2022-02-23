11f2f5f24016fd3b2f24ded654a09eb766b11762
Vitaly Lubart <vitaly.lubart@intel.com>
2022-02-23 21:42:53 +0200

    igsc: lib: implement oprom 4ids library api
    Implement oprom 4ids library api
    
    Signed-off-by: Vitaly Lubart <vitaly.lubart@intel.com>

--- lib/oprom.c
+++ lib/oprom.c
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
- * Copyright (C) 2020 Intel Corporation
+ * Copyright (C) 2020-2022 Intel Corporation
  */
 
 #include <stdint.h>
@@ -106,6 +106,32 @@ int igsc_image_oprom_iterator_reset(IN struct igsc_oprom_image *img)
     return IGSC_SUCCESS;
 }
 
+int igsc_image_oprom_iterator_reset_typed(IN struct igsc_oprom_image *img,
+                                          IN uint32_t request_type)
+{
+    enum igsc_oprom_type img_type;
+
+    if (img == NULL)
+    {
+        return IGSC_ERROR_INVALID_PARAMETER;
+    }
+    if (request_type != IGSC_OPROM_DATA && request_type != IGSC_OPROM_CODE)
+    {
+        return IGSC_ERROR_INVALID_PARAMETER;
+    }
+
+    img_type = image_oprom_get_type(img);
+    if ((img_type & request_type) == 0)
+    {
+        return IGSC_ERROR_NOT_SUPPORTED;
+    }
+
+    image_oprom_iterator_reset_4ids(img, request_type);
+
+    return IGSC_SUCCESS;
+}
+
+
 int igsc_image_oprom_iterator_next(IN struct igsc_oprom_image *img,
                                    OUT struct igsc_oprom_device_info *device)
 {
@@ -116,7 +142,6 @@ int igsc_image_oprom_iterator_next(IN struct igsc_oprom_image *img,
         return IGSC_ERROR_INVALID_PARAMETER;
     }
 
-
     /* If image has no data partition the iterator is not supported */
     img_type = image_oprom_get_type(img);
     if ((img_type & IGSC_OPROM_DATA) == 0)
@@ -125,6 +150,30 @@ int igsc_image_oprom_iterator_next(IN struct igsc_oprom_image *img,
     }
 
     return image_oprom_get_next(img, device);
+}
+
+int igsc_image_oprom_iterator_next_typed(IN struct igsc_oprom_image *img,
+                                         IN uint32_t request_type,
+                                         OUT struct igsc_oprom_device_info_4ids *device)
+{
+    enum igsc_oprom_type img_type;
+
+    if (img == NULL || device == NULL)
+    {
+        return IGSC_ERROR_INVALID_PARAMETER;
+    }
+    if (request_type != IGSC_OPROM_DATA && request_type != IGSC_OPROM_CODE)
+    {
+        return IGSC_ERROR_INVALID_PARAMETER;
+    }
+
+    img_type = image_oprom_get_type(img);
+    if ((img_type & request_type) == 0)
+    {
+        return IGSC_ERROR_NOT_SUPPORTED;
+    }
+
+    return image_oprom_get_next_4ids(img, request_type, device);
 }
 
 int igsc_image_oprom_count_devices(IN struct igsc_oprom_image *img,
@@ -146,6 +195,32 @@ int igsc_image_oprom_count_devices(IN struct igsc_oprom_image *img,
     }
 
     *count = image_oprom_count_devices(img);
+
+    return IGSC_SUCCESS;
+}
+
+int igsc_image_oprom_count_devices_typed(IN struct igsc_oprom_image *img,
+                                         IN uint32_t request_type,
+                                         OUT uint32_t *count)
+{
+    enum igsc_oprom_type img_type;
+
+    if (img == NULL || count == NULL)
+    {
+        return IGSC_ERROR_INVALID_PARAMETER;
+    }
+    if (request_type != IGSC_OPROM_DATA && request_type != IGSC_OPROM_CODE)
+    {
+        return IGSC_ERROR_INVALID_PARAMETER;
+    }
+
+    img_type = image_oprom_get_type(img);
+    if ((img_type & request_type) == 0)
+    {
+        return IGSC_ERROR_NOT_SUPPORTED;
+    }
+
+    *count = image_oprom_count_devices_4ids(img, request_type);
 
     return IGSC_SUCCESS;
 }
@@ -183,11 +258,59 @@ int igsc_image_oprom_supported_devices(IN struct igsc_oprom_image *img,
     return ret;
 }
 
+int igsc_image_oprom_supported_devices_typed(IN struct igsc_oprom_image *img,
+                                             IN uint32_t request_type,
+                                             OUT struct igsc_oprom_device_info_4ids *device,
+                                             IN OUT uint32_t *count)
+{
+    int ret;
+    uint32_t pos = 0;
+    enum igsc_oprom_type img_type;
+
+    if (img == NULL || device == NULL || count == NULL || *count == 0)
+    {
+        return IGSC_ERROR_INVALID_PARAMETER;
+    }
+    if (request_type != IGSC_OPROM_DATA && request_type != IGSC_OPROM_CODE)
+    {
+        return IGSC_ERROR_INVALID_PARAMETER;
+    }
+
+    img_type = image_oprom_get_type(img);
+    if ((img_type & request_type) == 0)
+    {
+        return IGSC_ERROR_NOT_SUPPORTED;
+    }
+
+    do
+    {
+        ret = image_oprom_get_next_4ids(img, request_type, &device[pos++]);
+    }
+    while (ret == IGSC_SUCCESS && pos < *count);
+
+    if (ret == IGSC_ERROR_DEVICE_NOT_FOUND)
+    {
+        ret = IGSC_SUCCESS;
+    }
+    *count = pos;
+
+    return ret;
+}
+
 static bool oprom_match_device(struct igsc_device_info *device,
                                struct igsc_oprom_device_info *oprom_device)
 {
     return (device->subsys_vendor_id == oprom_device->subsys_vendor_id) &&
            (device->subsys_device_id == oprom_device->subsys_device_id);
+}
+
+static bool oprom_match_device_4ids(struct igsc_device_info *device,
+                                    struct igsc_oprom_device_info_4ids *oprom_device)
+{
+    return (device->subsys_vendor_id == oprom_device->subsys_vendor_id) &&
+           (device->subsys_device_id == oprom_device->subsys_device_id) &&
+           (device->vendor_id == oprom_device->vendor_id) &&
+           (device->device_id == oprom_device->device_id);
 }
 
 
@@ -198,8 +321,10 @@ int igsc_image_oprom_match_device(IN struct igsc_oprom_image *img,
 {
     enum igsc_oprom_type img_type;
     struct igsc_oprom_device_info oprom_device;
+    struct igsc_oprom_device_info_4ids oprom_device_4ids;
     int ret;
     uint32_t count = 0;
+    bool image_4ids = true;
 
     if (img == NULL || device == NULL)
     {
@@ -221,36 +346,52 @@ int igsc_image_oprom_match_device(IN struct igsc_oprom_image *img,
         goto exit;
     }
 
-    /*
-     * The Code partition of OPROM is always supposed to match
-     * it the same across all derivatives .
-     */
-    if (request_type == IGSC_OPROM_CODE)
+    if (image_4ids)
     {
-        ret = IGSC_SUCCESS;
-        goto exit;
+        /* search the device list for a match */
+        while ((ret = image_oprom_get_next_4ids(img, request_type, &oprom_device_4ids)) == IGSC_SUCCESS)
+        {
+            if (oprom_match_device_4ids(device, &oprom_device_4ids))
+            {
+                ret = IGSC_SUCCESS;
+                goto exit;
+            }
+        }
+        ret = IGSC_ERROR_NOT_SUPPORTED;
     }
+    else
+    {
+        /*
+         * The Code partition of OPROM is always supposed to match
+         * it the same across all derivatives .
+         */
+        if (request_type == IGSC_OPROM_CODE)
+        {
+            ret = IGSC_SUCCESS;
+            goto exit;
+        }
 
-    /* A special case - device has no sub vendor */
-    if (device->subsys_device_id == 0 && device->subsys_vendor_id == 0)
-    {
-        /* empty device list is ok here */
-        igsc_image_oprom_count_devices(img, &count);
-        if (count == 0)
+        /* A special case - device has no sub vendor */
+        if (device->subsys_device_id == 0 && device->subsys_vendor_id == 0)
         {
-            ret = IGSC_SUCCESS;
-            goto exit;
+            /* empty device list is ok here */
+            igsc_image_oprom_count_devices(img, &count);
+            if (count == 0)
+            {
+                ret = IGSC_SUCCESS;
+                goto exit;
+            }
         }
-    }
-    /* search the device list for a match */
-    ret = IGSC_ERROR_NOT_SUPPORTED;
-    while ((ret = image_oprom_get_next(img, &oprom_device)) == IGSC_SUCCESS)
-    {
-        if (oprom_match_device(device, &oprom_device))
+        /* search the device list for a match */
+        while ((ret = image_oprom_get_next(img, &oprom_device)) == IGSC_SUCCESS)
         {
-            ret = IGSC_SUCCESS;
-            goto exit;
+            if (oprom_match_device(device, &oprom_device))
+            {
+                ret = IGSC_SUCCESS;
+                goto exit;
+            }
         }
+        ret = IGSC_ERROR_NOT_SUPPORTED;
     }
 
 exit:
