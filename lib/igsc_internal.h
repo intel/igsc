e0dac7bfb14d923d35180f9618e789b1b8bebad6
Alexander Usyskin <alexander.usyskin@intel.com>
2022-04-11 15:22:59 +0300

    igsc: lib: set power control to on during operations
    Set power control to on during operations in order to make sure
    some slow devices do not go to sleep during that period.
    
    Signed-off-by: Vitaly Lubart <vitaly.lubart@intel.com>
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- lib/igsc_internal.h
+++ lib/igsc_internal.h
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
- * Copyright (C) 2020 Intel Corporation
+ * Copyright (C) 2020-2022 Intel Corporation
  */
 #ifndef __IGSC_INTERNAL_H__
 #define __IGSC_INTERNAL_H__
@@ -16,6 +16,7 @@ struct igsc_lib_ctx {
     bool driver_init_called;          /**< Driver was initialized */
     struct gsc_fwu_img_layout layout; /**< Context for the image layout */
     uint32_t last_firmware_status;    /**< last status code returned from the firmware */
+    bool restore_power_control;       /**< need to restore power control for the device */
 };
 
 int gsc_fwu_buffer_validate(struct igsc_lib_ctx *lib_ctx,
---
+++ lib/power/igsc_power_windows.c
@@ -0,0 +1,21 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2022 Intel Corporation
+ */
+#include "msvc/config.h"
+#include "igsc_lib.h"
+#include "utils.h"
+
+int gsc_get_device_power_control(const char *devpath, uint8_t *power_control)
+{
+    (devpath);
+    *power_control = GSC_POWER_CONTROL_ON;
+    return IGSC_SUCCESS;
+}
+
+int gsc_set_device_power_control(const char *devpath, uint8_t power_control)
+{
+    (devpath);
+    (power_control);
+    return IGSC_SUCCESS;
+}
