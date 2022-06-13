48e84a2fa62d3996e453c560f66adb456f7a73ca
Alexander Usyskin <alexander.usyskin@intel.com>
2022-06-13 15:26:17 +0300

    igsc: conan: clean conanfile
    Rename object to igsc one.
    Drop unneeded os reference.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- conanfile.py
+++ conanfile.py
@@ -2,12 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (C) 2021-2022 Intel Corporation
 from conans import ConanFile
-import os
 
-class LMSConan(ConanFile):
+class IgscConan(ConanFile):
     name = "igsc"
     generators = "cmake", "cmake_find_package", "visual_studio"
-    settings = "os"
 
     def requirements(self):
         self.requires("metee/3.1.3@mesw/stable")
