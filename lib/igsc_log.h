3378c1c82f46d47c392c457373255f93dbd005b8
Vitaly Lubart <vitaly.lubart@intel.com>
2023-04-25 19:18:23 +0300

    igsc: lib: add trace log
    Add trace log in addition to the error and debug logs
    
    Signed-off-by: Vitaly Lubart <vitaly.lubart@intel.com>

--- lib/igsc_log.h
+++ lib/igsc_log.h
@@ -26,12 +26,14 @@
        }
 
        #define error_print(fmt, ...) debug_print(fmt, ##__VA_ARGS__)
+       #define trace_print(fmt, ...) debug_print(fmt, ##__VA_ARGS__)
 
     #else /* WIN32 */
 
         #include <syslog.h>
         #define debug_print(fmt, ...) syslog(LOG_DEBUG, fmt, ##__VA_ARGS__)
         #define error_print(fmt, ...) syslog(LOG_ERR, fmt, ##__VA_ARGS__)
+        #define trace_print(fmt, ...) syslog(LOG_INFO, fmt, ##__VA_ARGS__)
 
     #endif /* _WIN32 */
 
@@ -39,14 +41,19 @@
         #include <stdio.h>
         #define debug_print(fmt, ...) fprintf(stdout, fmt, ##__VA_ARGS__)
         #define error_print(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
+        #define trace_print(fmt, ...) fprintf(stdout, fmt, ##__VA_ARGS__)
 #endif /* SYSLOG */
 
 #if defined(DEBUG) || defined(_DEBUG)
 #define gsc_debug(_fmt_, ...) \
     debug_print(PACKAGE_LOG_NAME ": (%s:%s():%d) " _fmt_, \
                 __FILE__, __func__, __LINE__,  ##__VA_ARGS__)
+#define gsc_trace(_fmt_, ...) \
+    trace_print(PACKAGE_LOG_NAME ": (%s:%s():%d) " _fmt_, \
+                __FILE__, __func__, __LINE__,  ##__VA_ARGS__)
 #else
     #define gsc_debug(_x_, ...)
+    #define gsc_trace(_x_, ...)
 #endif /* PRINTS_ENABLE */
 
 #define gsc_error(_fmt_, ...) \
